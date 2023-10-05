42d933224efc757c36fe7ebb49de34e8f7cb004c
Adams <147047497+DEVACMK@users.noreply.github.com>
2023-10-05 18:34:23 +0000

    admin.py

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Model3D)
+class Model3DAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "image",
+        "views",
+        "uploads",
+    )
+
+@admin.register(models.Badge)
+class BadgeAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "critaire",
+    )
+
+@admin.register(models.UserBadge)
+class UserBadgeAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "badge",
+        "date_awarded",
+    )
